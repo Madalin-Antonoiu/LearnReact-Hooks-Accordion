bb57faf60bf76d051f6411100abc7bb8b2dcf753
@madalin-antonoiu <20832842+Madalin-Antonoiu@users.noreply.github.com>
2020-12-22 10:52:13 +0000

    Throttling Api calls! Removing previous timer on new type :)

--- src/components/Search.js
+++ src/components/Search.js
@@ -5,11 +5,6 @@ const Search = ({ label, data }) => {
   const [term, setTerm] = useState("programming");
   const [results, setResults] = useState([]);
 
-  const onFormSubmit = (event) => {
-    event.preventDefault();
-    data(term); //pass it as props to the parent
-  };
-
   useEffect(() => {
     const search = async () => {
       //https://en.wikipedia.org/w/api.php//?action=query&list=search&format=json&origin=*&srsearch=programming
@@ -25,11 +20,16 @@ const Search = ({ label, data }) => {
       setResults(data.query.search);
     };
 
-    // Possible too, if i dont want to provide an initial search term
-    if (term) {
-      search();
-    }
-    //search();
+    // Throttling API calls for live-search!
+    const timeoutId = setTimeout(() => {
+      if (term) {
+        search();
+      }
+    }, 500);
+
+    return () => {
+      clearTimeout(timeoutId);
+    };
   }, [term]);
 
   const renderedResults = results.map((result) => {
@@ -39,6 +39,8 @@ const Search = ({ label, data }) => {
           <a
             href={`https://en.wikipedia.org?curid=${result.pageid}`}
             className="ui button"
+            target="_blank"
+            rel="noopener noreferrer"
           >
             Go
           </a>
@@ -53,18 +55,16 @@ const Search = ({ label, data }) => {
 
   return (
     <div className="search-bar ui segment">
-      <form className="ui form">
-        <div className="field">
-          <label>{label}</label>
-          <br />
-          <input
-            value={term}
-            onChange={(e) => setTerm(e.target.value)}
-            type="text"
-            placeholder="Type and press Enter"
-          />
-        </div>
-      </form>
+      <div className="field">
+        <label>{label}</label>
+        <br />
+        <input
+          value={term}
+          onChange={(e) => setTerm(e.target.value)}
+          type="text"
+          placeholder="Search Wikipedia as you type"
+        />
+      </div>
 
       <div className="ui celled list">{renderedResults}</div>
     </div>
