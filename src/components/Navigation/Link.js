ff4bb54834f1f5fa47296c2927be8933c608a877
@madalin-antonoiu <20832842+Madalin-Antonoiu@users.noreply.github.com>
2021-01-04 15:08:04 +0000

    Step 2 - Keeping the URL in sync(window.history.pushState)

--- src/components/Navigation/Link.js
+++ src/components/Navigation/Link.js
@@ -3,6 +3,7 @@ import React from "react";
 export default ({ className, href, children }) => {
   const onClick = (event) => {
     event.preventDefault(); // prevent full page reload
+    window.history.pushState({}, "", href); // needed for keeping url in sync, after preventing default link behavior
   };
 
   return (
