f2e94c97807503a231ce126d5ce6c2452e884ca0
@madalin-antonoiu <20832842+Madalin-Antonoiu@users.noreply.github.com>
2021-01-04 15:30:51 +0000

    FINISHED + Restoring CTRL+ Click opens new page.

--- src/components/Navigation/Link.js
+++ src/components/Navigation/Link.js
@@ -2,6 +2,11 @@ import React from "react";
 
 export default ({ className, href, children }) => {
   const onClick = (event) => {
+    //Restoring CTRL+ Click opens new page
+    if (event.metaKey || event.ctrlKey) {
+      return;
+    }
+
     event.preventDefault(); // prevent full page reload
     window.history.pushState({}, "", href); // needed for keeping url in sync, after preventing default link behavior
 
