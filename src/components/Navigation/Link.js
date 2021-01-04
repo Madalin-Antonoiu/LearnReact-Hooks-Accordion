381d75832b1de88e86dd0741226adae9aee26984
@madalin-antonoiu <20832842+Madalin-Antonoiu@users.noreply.github.com>
2021-01-04 15:16:55 +0000

    Step 3- Emitting Custom Navigation Event

--- src/components/Navigation/Link.js
+++ src/components/Navigation/Link.js
@@ -4,6 +4,9 @@ export default ({ className, href, children }) => {
   const onClick = (event) => {
     event.preventDefault(); // prevent full page reload
     window.history.pushState({}, "", href); // needed for keeping url in sync, after preventing default link behavior
+
+    const navEvent = new PopStateEvent("popstate");
+    window.dispatchEvent(navEvent);
   };
 
   return (
--- src/components/Navigation/Route.js
+++ src/components/Navigation/Route.js
@@ -1,9 +1,25 @@
 // Not used any JSX yet, no need to import React
 //import React from "react";
+import { useEffect } from "react";
 
 // children is received from children component passed to Route, aka <Dropdown /> etc
 const Route = ({ path, children }) => {
+  useEffect(() => {
+    const onLocationChange = () => {
+      console.log("Location Change");
+    };
+
+    window.addEventListener("popstate", onLocationChange);
+
+    // Clean up the event listener when we stop showing the component on the screen
+    return () => {
+      window.removeEventListener("popstate", onLocationChange);
+    };
+  }, []); // Reminder: [] means run only once on render
+
   return window.location.pathname === path ? children : null;
 };
 
 export default Route;
+
+// we will get 4 calls of console log but that`s fine for this stage
